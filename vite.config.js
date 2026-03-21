d15c88218d3849c337da64c41f81da04008fd2a0
SallyFeng1223 <129814925+SallyFeng1223@users.noreply.github.com>
2026-03-21 15:08:33 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// 這裡的 base 必須填入您 GitHub 專案的精確名稱，前後都要有斜線
 export default defineConfig({
   plugins: [react()],
+  base: '/jept-game/', 
 })
